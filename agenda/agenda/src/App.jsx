1d556d22e5dcf60de2f56a7eaabdc7f8f6c7b68d
pabli <uo282440@uniovi.es>
2025-07-25 22:27:12 +0200

    part2 c finished

--- agenda/agenda/src/App.jsx
+++ agenda/agenda/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import CustomForm from './components/CustomForm'
 import People from './components/People'
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,
-      number: '985 76 84 56',
-      id: 1
-    },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameToFilter, setNameToFilter] = useState('')
 
+  const hook = () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then( response =>
+      setPersons(response.data)
+    )
+  }
+
+  useEffect(hook,[])
   
 
   const peopleToShow = nameToFilter === '' ? persons : persons.filter(
     person => person.name.toLowerCase().startsWith(nameToFilter.toLowerCase())
   )
   
-  //LO DEJAMOS EN 2C OBTENIENDO DATOS DEL SERVIDOR
 
   //controllers
 
--- part2/examples/part2/src/App.jsx
+++ part2/examples/part2/src/App.jsx
@@ -1,12 +1,41 @@
 import Note from './components/Note'
 import Course from './components/Course'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const App = (props) => {
+const App = () => {
 
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(    'a new note...'  )
   const [showAll, setShowAll] = useState(true)
+
+  /*
+  useEffect( () => { 
+
+    console.log('effect')    
+    axios
+      .get('http://localhost:3001/notes')
+
+      .then(response => { 
+        console.log('promise fulfilled')        
+        setNotes(response.data)      
+      })
+      
+  }, [])  
+        
+  console.log('render', notes.length, 'notes')*/
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
